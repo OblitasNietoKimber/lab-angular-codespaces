55c11773109f801c214b5a07bd24b3230ad24a14
OblitasNietoKimber <koblitas2002@gmail.com>
2026-05-21 16:48:41 +0000

    creacion del proyecto

---
+++ mi-app-angular/src/app/services/producto.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  private apiUrl = 'http://localhost:3000/productos';
+
+  constructor(private http: HttpClient) { }
+
+  listarProductos(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
+
+  obtenerProductoPorId(id: number): Observable<Producto> {
+    return this.http.get<Producto>(`${this.apiUrl}/${id}`);
+  }
+
+  registrarProducto(producto: Producto): Observable<Producto> {
+    return this.http.post<Producto>(this.apiUrl, producto);
+  }
+
+  actualizarProducto(producto: Producto): Observable<Producto> {
+    return this.http.put<Producto>(`${this.apiUrl}/${producto.id}`, producto);
+  }
+
+  eliminarProducto(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
